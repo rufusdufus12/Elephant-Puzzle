4307ae1ab23e670ccfea5211ddbb7f8ccf96bf62
rufusdufus12 <shinjudaiyamondo@yahoo.com>
2017-03-06 01:01:24 -0600

    create file

---
+++ Elephant Puzzle/Elephant Puzzle/Elephant Puzzle.cpp
@@ -0,0 +1,17 @@
+// Elephant Puzzle.cpp : Defines the entry point for the console application.
+//
+#include <iostream>
+#include "stdafx.h"
+
+using namespace std;
+int main()
+{
+	int ele;
+
+	cout << "How many elephants are there?" << endl;
+	cin >> ele;
+
+
+    return 0;
+}
+
