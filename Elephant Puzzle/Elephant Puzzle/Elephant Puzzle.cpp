8b1a22e0e32fa9728f6ae625cc190d1cce4d3823
Amy <ahdong@coe.edu>
2017-03-07 16:50:01 -0600

    IT WORKS!!!

--- Elephant Puzzle/Elephant Puzzle/Elephant Puzzle.cpp
+++ Elephant Puzzle/Elephant Puzzle/Elephant Puzzle.cpp
@@ -1,11 +1,12 @@
 // Elephant Puzzle.cpp : Defines the entry point for the console application.
 //
 #include <iostream>
+#include <vector>
 #include "stdafx.h"
 
 
 using namespace std;
-void move(int num) {
+/*void move(int num) {
 	if (num == 1) {
 		cout << "Turn." << endl;
 	}
@@ -44,6 +45,109 @@ void slideLeft(char *a, int ele) {
 		cout << a[i];
 	}
 	cout << endl;
+}*/
+
+/*int slid = 5;
+vector<char> turn(int pos, vector<char> a) {
+	cout << "Turn" << endl;
+	if (a[pos] == 'v') {
+		a[pos] = 'h';
+	}
+	else if (a[pos] == 'h')
+	{
+		a[pos] = 'v';
+	}
+	return a;
+}
+
+vector<char> slide(int slidePos, int pos, vector<char> a) {
+	cout << "slide" << endl;
+	//int slid = 5;
+	if (slid == slidePos) {
+		a = turn(pos, a);
+	}
+	else
+	{
+		if (slid < slidePos) slid = (slid + 5 < slidePos) ? slid + 5 : slidePos;
+		else slid = (slid - 5 < slidePos) ? slid - 5 : slidePos;
+		///some missing stuff here
+		a = slide(slidePos, pos, a);
+	}
+
+	return a;
+}
+
+vector<char> move(int pos, vector<char> a) {
+	cout << "move" << endl;
+	if (pos == 0) {
+		a = slide(5+32*pos, pos, a);
+	}
+	else if (a[pos - 1] == 'h') {
+		a = move(pos - 1, a);
+	}
+	else
+	{
+		int i;
+		for (i = pos - 2; i >= 0 && a[i] == 'h'; i--);
+		if (i >= 0) {
+			a = move(i, a);
+		}
+		else a = slide(5+32*pos, pos, a);
+	}
+	return a;
+}*/
+
+vector<char> turn(vector<char> a, int tPos) {
+	if (a[tPos] == 'h') {
+		a[tPos] = 'v';
+	}
+	else if (a[tPos] == 'v') {
+		a[tPos] = 'h';
+	}
+
+	cout << "Turn." << endl;
+	return a;
+}
+
+vector<char> slideRight(vector<char> a, int tPos) {
+	vector<char>::iterator it = a.begin();
+	if (a[tPos + 1] != 'v') {
+		cout << "Slide right." << endl;
+		a.insert(it, ' ');
+		return slideRight(a, tPos);
+	}
+	else if (a[tPos+1] == 'v') {
+		a = turn(a, tPos);
+		return a;
+	}
+}
+
+vector<char> slideLeft(vector<char> a, int tPos) {
+	vector<char>::iterator it = a.begin();
+	if (a[0] == ' ') {
+		cout << "Slide left." << endl;
+		a.erase(it);
+		return slideLeft(a, tPos);
+	}
+	else {
+		a = turn(a, tPos);
+		return a;
+	}
+}
+
+vector<char> move(vector<char> a, int tPos, vector<char> correct) {	
+	if (a == correct) {
+		cout << "The elephants are all lined up!" << endl;
+		return a;
+	}
+	else if (a[0] != ' ') {
+		a = slideRight(a, tPos);
+		return move(a, tPos, correct);
+	}
+	else {
+		a = slideLeft(a, tPos);
+		return move(a, tPos, correct);
+	}
 }
 
 int main() {
@@ -57,20 +161,28 @@ int main() {
 		cin >> ele;
 	}
 
-	char *elephants = new char [ele * 2];
-	int t = ele;
-	int s = ele + 1;
+	vector<char> elephants, correct;
+	int tPos = ele - 1;
 
+	// build feeding area for the elephants
 	for (int i(0); i < ele; i++) {
-		elephants[i] = 'v';
+		elephants.push_back('v');
+		correct.push_back('h');
 	}
-	for (int i(ele); i < sizeof(elephants); i++) {
-		elephants[i] = NULL;
-	}
+	elephants.push_back(' ');
+	correct.push_back(' ');
 
-	cout << elephants << endl;
-	slideRight(elephants, ele);
-	slideLeft(elephants, ele);
+	if (ele % 2 == 1) {
+		elephants = turn(elephants, tPos);
+	}
+	
+	elephants = move(elephants, tPos, correct);
+
+	// let's see the pretty elephants
+	for (int i(0); i < elephants.size(); i++) {
+		cout << elephants[i];
+	}
+	cout << endl;
 
 	return 0;
 }
